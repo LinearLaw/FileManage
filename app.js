e5baf3981d39bd8a386421eb57e555dc7038202b
LinearLaw <570479543@qq.com>
2018-11-05 22:14:16 +0800

    complete module of path

--- app.js
+++ app.js
@@ -4,14 +4,33 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 
-app.use(express.static(__dirname+"./webapp"));
+app.use(express.static("./webapp"));
+app.use(express.static("./FileContainer"));
 
 app.get("/file",(req,res)=>{
     console.log(req.query);
     fileCtrl.getFile(req.query.dir).then((files)=>{
+        let arr = [];
+        files.map((item,index)=>{
+            let direction = path.resolve(__dirname ,"./FileContainer"+req.query.dir + "/"+item);
+            let stat = fs.statSync(direction);
+            if(stat.isDirectory()){
+                //是文件
+                arr.push({
+                    fileName:item,
+                    isFile:false
+                })
+            }else{
+                 //是文件夹
+                arr.push({
+                    fileName:item,
+                    isFile:true
+                })
+            }
+        })
         res.send({
             basePath:req.query.dir,
-            data:files,
+            data:arr,
             status:"success"
         });
     });
@@ -30,7 +49,6 @@ const fileCtrl = {
                     throw err;
                 }
                 // files是一个数组,每个元素是此目录下的文件或文件夹的名称
-                console.log(files);
                 resolve(files);
             });
         },(err)=>{console.log(err)});
