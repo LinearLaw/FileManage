e2e8a6bf89d41b0342ef801266a0307070620a6d
LinearLaw <570479543@qq.com>
2018-12-23 16:47:55 +0800

    add log of request

--- app.js
+++ app.js
@@ -6,42 +6,7 @@ const fs = require("fs");
 const path = require("path");
 
 const PORT_NUM = 8008;
-
-app.use(express.static("./webapp"));
-app.use(express.static("./FileContainer"));
-
-app.get("/file",(req,res)=>{
-    // console.log(req.query);
-    // console.log(fileCtrl.getLocalIp());
-    fileCtrl.getFile(req.query.dir).then((files)=>{
-        let arr = [];
-        files.map((item,index)=>{
-            let direction = path.resolve(__dirname ,"./FileContainer"+req.query.dir + "/"+item);
-            let stat = fs.statSync(direction);
-            if(stat.isDirectory()){
-                //是文件
-                arr.push({
-                    fileName:item,
-                    isFile:false
-                })
-            }else{
-                 //是文件夹
-                arr.push({
-                    fileName:item,
-                    isFile:true
-                })
-            }
-        })
-        res.send({
-            basePath:`${fileCtrl.getLocalIp()}:${PORT_NUM}`,
-            data:arr,
-            status:"success"
-        });
-    });
-
-});
-
-const fileCtrl = {
+const TOOLS = {
     getFile(filePath){
         return new Promise((resolve,reject)=>{
             fs.readdir(path.resolve(__dirname,'./FileContainer/'+filePath), function(err, files) {
@@ -64,8 +29,52 @@ const fileCtrl = {
             }  
         }    
         return map;
+    },
+    //获取请求来源的IP
+    getReqRemoteIp(req){
+        return req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
     }
 }
+
+app.use(express.static("./webapp"));
+app.use(express.static("./FileContainer"));
+
+app.get("/file",(req,res)=>{
+    // console.log(req.query);
+    // console.log(TOOLS.getLocalIp());
+    console.log(`收到了来自${TOOLS.getReqRemoteIp(req)} ${req.url}的请求`);
+    TOOLS.getFile(req.query.dir).then((files)=>{
+        let arr = [];
+        files.map((item,index)=>{
+            let direction = path.resolve(__dirname ,"./FileContainer"+req.query.dir + "/"+item);
+            let stat = fs.statSync(direction);
+            if(stat.isDirectory()){
+                //是文件
+                arr.push({
+                    fileName:item,
+                    isFile:false
+                })
+            }else{
+                 //是文件夹
+                arr.push({
+                    fileName:item,
+                    isFile:true
+                })
+            }
+        })
+        res.send({
+            basePath:`${TOOLS.getLocalIp()}:${PORT_NUM}`,
+            data:arr,
+            status:"success"
+        });
+    });
+
+});
+
+
 
 app.listen(PORT_NUM,()=>{
     console.log(`project prot ${PORT_NUM}`);       
