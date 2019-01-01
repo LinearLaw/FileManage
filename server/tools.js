5daf47cd0b94a79278fceac947ed26c509f26056
LinearLaw <570479543@qq.com>
2019-01-01 21:31:18 +0800

    Fix bug of insert ip function

--- server/tools.js
+++ server/tools.js
@@ -1,10 +1,11 @@
 const os = require("os")
 const fs = require("fs");
+const path = require("path");
  
 module.exports = {
     getFile(filePath){
         return new Promise((resolve,reject)=>{
-            fs.readdir(path.resolve(__dirname,'./FileContainer/'+filePath), function(err, files) {
+            fs.readdir(path.resolve(__dirname,'../FileContainer/'+filePath), function(err, files) {
                 if (err) {
                     reject(err);
                     throw err;
