aa949a56130fa4d7ac13c7c4bbb5b3a490a7adb0
LinearLaw <570479543@qq.com>
2019-02-13 17:24:42 +0800

    Add LAN ip as base path

--- server/config.js
+++ server/config.js
@@ -7,7 +7,8 @@ const LAN_IP = tools.getLocalIp();
 
 const STATIC_DIR = `./webapp/v2/dist`; // webapp基础路径
 const SHARE_DIR = `G:/`; // 静态文件资源基础路径
-const BASE_PATH = `http://${CURRENT_IP}:${PORT_NUM}`;
+const BASE_PATH = `http://${LAN_IP}:${PORT_NUM}`;
+const BASE_PATH_v2 = `http://${CURRENT_IP}:${PORT_NUM}`;
 
 module.exports = {
     PORT_NUM,
@@ -16,4 +17,5 @@ module.exports = {
     STATIC_DIR,
     SHARE_DIR,
     BASE_PATH,
+    BASE_PATH_v2,
 }
